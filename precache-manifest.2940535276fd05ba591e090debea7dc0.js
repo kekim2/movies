8016f2f074abc2069739cc2da7bb1dfbfa9382d8
KunEui <kuney930216@gmail.com>
2019-10-22 14:54:47 -0400

    Updates

---
+++ precache-manifest.2940535276fd05ba591e090debea7dc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7655f8e94cde5422fd09d3241f8ae028",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "4bca62155e2a34e3d4c5",
+    "url": "/movies/static/css/main.2ce72dcc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/movies/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4bca62155e2a34e3d4c5",
+    "url": "/movies/static/js/main.e8d53c98.chunk.js"
+  },
+  {
+    "revision": "04129ba559ae92aa086e",
+    "url": "/movies/static/js/runtime~main.712e9881.js"
+  }
+]);
